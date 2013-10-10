738593bc525e0ab43f3c75dcb2d634cfb46f7a46
Adigo Atabo <Adigo@Adigos-MacBook-Air.local>
2013-10-10 16:18:03 -0500

    Index and create blog views revised.  Routing for create blog form in progress.  Database dropped and re-established using three models: join table in addition to posts and tags.

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -15,6 +15,26 @@ get '/posts' do
   erb :index
 end
 
+get '/posts/create' do
+  erb :create_post
+end
+
+post '/posts/create' do
+
+  tags = params[:tag].split(", ")
+  copy = tags.dup
+  until copy.length == 0
+    Tag.create(name: copy.pop)
+  end
+
+  tags.each do |tag|
+    source = Tag.find_by_name(tag)
+    source.id
+  end
+  post = Post.create(title: params[:title], body: params[:body])
+  erb :index
+end
+
 get '/posts/display_one' do
   @post = Post.find_by_title(params[:title])
   erb :display_one_post
